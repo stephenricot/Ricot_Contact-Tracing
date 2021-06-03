fdd28ba73c137f9feb5cd0c05097e40eace2e07e
stephen ricot <stephenricot@gmail.com>
2021-06-03 12:49:56 +0800

    Added New Features: - Buttons - Input and Output featured codes

--- Ricot_Contact Tracing/Form1.Designer.cs
+++ Ricot_Contact Tracing/Form1.Designer.cs
@@ -1,4 +1,6 @@
 ﻿
+using System;
+
 namespace Ricot_Contact_Tracing
 {
     partial class Form1
@@ -30,9 +32,17 @@ namespace Ricot_Contact_Tracing
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
+            this.contact_input = new System.Windows.Forms.TextBox();
+            this.address_input = new System.Windows.Forms.TextBox();
+            this.age_input = new System.Windows.Forms.TextBox();
+            this.name_input = new System.Windows.Forms.TextBox();
+            this.contact = new System.Windows.Forms.Label();
+            this.address = new System.Windows.Forms.Label();
+            this.age = new System.Windows.Forms.Label();
+            this.name = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel3.SuspendLayout();
@@ -40,20 +50,12 @@ namespace Ricot_Contact_Tracing
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.Bisque;
+            this.panel1.BackColor = System.Drawing.Color.LightSkyBlue;
             this.panel1.Controls.Add(this.label1);
             this.panel1.Location = new System.Drawing.Point(8, 9);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(951, 109);
             this.panel1.TabIndex = 0;
-            // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.Color.PeachPuff;
-            this.panel2.Location = new System.Drawing.Point(8, 131);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(470, 378);
-            this.panel2.TabIndex = 1;
             // 
             // label1
             // 
@@ -65,43 +67,168 @@ namespace Ricot_Contact_Tracing
             this.label1.TabIndex = 0;
             this.label1.Text = "Contact Tracing App";
             // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.panel2.BackgroundImage = global::Ricot_Contact_Tracing.Properties.Resources._3214373;
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel2.Location = new System.Drawing.Point(8, 131);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(329, 378);
+            this.panel2.TabIndex = 1;
+            // 
             // panel3
             // 
-            this.panel3.BackColor = System.Drawing.Color.PeachPuff;
+            this.panel3.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.panel3.Controls.Add(this.contact_input);
+            this.panel3.Controls.Add(this.address_input);
+            this.panel3.Controls.Add(this.age_input);
+            this.panel3.Controls.Add(this.name_input);
+            this.panel3.Controls.Add(this.contact);
+            this.panel3.Controls.Add(this.address);
+            this.panel3.Controls.Add(this.age);
+            this.panel3.Controls.Add(this.name);
             this.panel3.Controls.Add(this.button1);
-            this.panel3.Location = new System.Drawing.Point(489, 131);
+            this.panel3.Location = new System.Drawing.Point(356, 131);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(470, 378);
+            this.panel3.Size = new System.Drawing.Size(603, 378);
             this.panel3.TabIndex = 2;
+            // 
+            // contact_input
+            // 
+            this.contact_input.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.contact_input.Location = new System.Drawing.Point(227, 180);
+            this.contact_input.Name = "contact_input";
+            this.contact_input.Size = new System.Drawing.Size(333, 45);
+            this.contact_input.TabIndex = 13;
+            this.contact_input.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // address_input
+            // 
+            this.address_input.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.address_input.Location = new System.Drawing.Point(213, 128);
+            this.address_input.Name = "address_input";
+            this.address_input.Size = new System.Drawing.Size(347, 45);
+            this.address_input.TabIndex = 12;
+            this.address_input.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // age_input
+            // 
+            this.age_input.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.age_input.Location = new System.Drawing.Point(138, 74);
+            this.age_input.Name = "age_input";
+            this.age_input.Size = new System.Drawing.Size(91, 45);
+            this.age_input.TabIndex = 11;
+            this.age_input.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // name_input
+            // 
+            this.name_input.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.name_input.Location = new System.Drawing.Point(169, 20);
+            this.name_input.Name = "name_input";
+            this.name_input.Size = new System.Drawing.Size(343, 45);
+            this.name_input.TabIndex = 10;
+            this.name_input.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // contact
+            // 
+            this.contact.AutoSize = true;
+            this.contact.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.contact.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.contact.Location = new System.Drawing.Point(23, 183);
+            this.contact.Name = "contact";
+            this.contact.Size = new System.Drawing.Size(175, 38);
+            this.contact.TabIndex = 4;
+            this.contact.Text = "Contact/s:";
+            // 
+            // address
+            // 
+            this.address.AutoSize = true;
+            this.address.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.address.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.address.Location = new System.Drawing.Point(23, 131);
+            this.address.Name = "address";
+            this.address.Size = new System.Drawing.Size(155, 38);
+            this.address.TabIndex = 3;
+            this.address.Text = "Address:";
+            // 
+            // age
+            // 
+            this.age.AutoSize = true;
+            this.age.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.age.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.age.Location = new System.Drawing.Point(23, 77);
+            this.age.Name = "age";
+            this.age.Size = new System.Drawing.Size(88, 38);
+            this.age.TabIndex = 2;
+            this.age.Text = "Age:";
+            // 
+            // name
+            // 
+            this.name.AutoSize = true;
+            this.name.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.name.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.name.Location = new System.Drawing.Point(23, 23);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(118, 38);
+            this.name.TabIndex = 1;
+            this.name.Text = "Name:";
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.WhiteSmoke;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(145, 295);
+            this.button1.Location = new System.Drawing.Point(200, 310);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(194, 54);
+            this.button1.Size = new System.Drawing.Size(243, 54);
             this.button1.TabIndex = 0;
+            this.button1.TabStop = false;
             this.button1.Text = "ENTER";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.enter_click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SaddleBrown;
+            this.BackgroundImage = global::Ricot_Contact_Tracing.Properties.Resources._187161;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(966, 517);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Contact Tracing App";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.ResumeLayout(false);
 
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            
         }
 
         #endregion
@@ -111,6 +238,14 @@ namespace Ricot_Contact_Tracing
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label contact;
+        private System.Windows.Forms.Label address;
+        private System.Windows.Forms.Label age;
+        private System.Windows.Forms.Label name;
+        private System.Windows.Forms.TextBox contact_input;
+        private System.Windows.Forms.TextBox address_input;
+        private System.Windows.Forms.TextBox age_input;
+        private System.Windows.Forms.TextBox name_input;
     }
 }
 
--- Ricot_Contact Tracing/Form1.cs
+++ Ricot_Contact Tracing/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,33 @@ namespace Ricot_Contact_Tracing
 {
     public partial class Form1 : Form
     {
+        private object txt;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void enter_click(object sender, EventArgs e)
+        {
+            StreamWriter OutputFile;
+            OutputFile = File.CreateText("Output.txt");
+
+            OutputFile.Write(value: name_input.Text);
+            OutputFile.Write(value: age_input.Text);
+            OutputFile.Write(value: address_input.Text);
+            OutputFile.Write(value: contact_input.Text);
+            OutputFile.Close();
         }
     }
 }
